4e696caccb78ed47293aff967c7c3036d8139038
JayMFrimmel <jayfrimmel@yahoo.com>
2017-02-03 12:06:20 -0500

    added JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
+var hangmanGame = {
+
 // establish initial array
-var gameWords = ["Statue of Liberty", "White House", "Golden Gate Bridge", "Niagara Falls", "Space Needle"];
+	gameWords = ["Statue of Liberty", "White House", "Golden Gate Bridge", "Niagara Falls", "Space Needle"];
 
 // keep track of words in play, letters in the words, guesses made and remaining, wins
 
@@ -34,4 +36,109 @@ updatePage: function(letter) {
 			this.restartGame();
 		}
 	}
+},
+
+updateGuesses: function(letter) {
+	//if the letter is not in the guessedLetters array
+		//and
+		//the letter is not in the lettersOfTheWord array
+		//then
+		//make guesses go down
+
+	if ((this.guessedLetters.indexOf(letter) == -1) && (this.lettersOfTheWords.indexOf(letter) == -1)) {
+		this.guessedLetters.push(letter);
+		this.guessesLeft--;
+		document.querySelector('#guesses-remaining').innerHTML = this.guessesLeft;
+		document.querySelector("#guessed-letters").innerHTML = this.guessedLetters.join(', ');
+	}
+},
+processUpdateTotalGuesses: function() {
+		this.totalGuesses = this.lettersOfTheWords.length + 5;
+		this.guessesLeft = this.totalGuesses;
+
+		// ---Render the guesses left
+		document.querySelector('#guesses-remaining').innerHTML = this.guessesLeft;
+	},
+	updateMatchedLetters: function(letter){
+		for (var i = 0; i < this.lettersOfTheWords.length; i++) {
+			if ((letter === this.lettersOfTheWords[i]) && (this.matchedLetters.indexOf(letter) == -1)){
+				this.matchedLetters.push(letter);
+			};
+		};
+	},
+
+	// rebuild the words
+	rebuildWordView: function() {
+		var wordView = "";
+
+		for(var i=0; i < this.lettersOfTheWords.length; i++){
+			if (this.matchedLetters.indexOf(this.lettersOfTheWords[i]) != -1){
+				wordView += this.lettersOfTheWords[i];				
+			}else{
+				wordView += '&nbsp;_&nbsp;';
+			}
+		}
+
+		document.querySelector('#current-word').innerHTML = wordView;
+
+	},
+
+	// game restart
+	restartGame : function(){
+		document.querySelector('#guessed-letters').innerHTML = '';
+		this.wordsInPlay = null;
+		this.lettersOfTheWords = [];
+		this.matchedLetters = [];
+		this.guessedLetters = [];
+		this.guessesLeft = 0;
+		this.totalGuesses = 0;
+		this.letterGuessed = null;
+		this.setupGame();
+		this.rebuildWordView();
+	},
+
+	//update wins function
+	updateWins: function() {
+
+		//this won't work for words with double or triple letters
+			var lettersOfTheWordClone = this.lettersOfTheWords.slice(); //clones the array
+			this.matchedLetters.sort().join('') == lettersOfTheWordsClone.sort().join('')
+
+		if (this.matchedLetters.length == 0){
+			var win = false;
+		}else{
+			var win = true
+		}
+		
+		for (var i=0; i < this.lettersOfTheWords.length; i++){
+			if (this.matchedLetters.indexOf(this.lettersOfTheWords[i]) == -1){
+				win = false;
+			}
+		}
+
+		if (win == true){
+			this.wins =  this.wins + 1;
+			
+			document.querySelector('#wins').innerHTML = this.wins;
+
+			//document.querySelector('#music').innerHTML = this.wordsToPick[this.wordInPlay].song + " By " + this.wordInPlay;
+
+			//document.querySelector('#bandDiv').innerHTML = '<img class="bandImage" src="images/' + this.wordsToPick[this.wordInPlay].picture + '" alt="' + this.wordsToPick[this.wordInPlay].song + '">';
+
+			//var audio = new Audio(this.wordsToPick[this.wordInPlay].preview);
+			//audio.play();
+
+
+			return true;
+		}else{
+			return false;
+		}
+	}
+};
+
+hangmanGame.setupGame();
+
+document.onkeyup = function(event) {
+	hangmanGame.letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	hangmanGame.updatePage(hangmanGame.letterGuessed);
 }
